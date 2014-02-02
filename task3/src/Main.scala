c2933b1bf5d46a83af0aaf463c8359377a95dbe9
jonyrock <ilms@live.ru>
2014-02-02 23:16:50 +0400

    rm main

--- task3/src/Main.scala
+++
@@ -1,9 +0,0 @@
-object Main extends App {
-  val program =
-    """val a = {123}
-      |val c = {"adfasdf"}
-      |val b = {val c = {"hello"}}
-    """.stripMargin
-//  val program ="""val a = {123} val c = {"12sadasdfsadf"}""".stripMargin
-  println(ASTBuilder.buildAST(program).toString)
-}
